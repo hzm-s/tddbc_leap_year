99778494e942a6625115b9b92563b560a3e77aee
haazime <h4zime@gmail.com>
2014-09-06 18:23:05 +0900

    4で割り切れる年はうるう年

--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -11,4 +11,24 @@ RSpec.describe 'うるう年の判定' do
     let(:year) { 2014 }
     it { is_expected.to be_falsey }
   end
+
+  context '2004年の場合' do
+    let(:year) { 2004 }
+    it { is_expected.to be_truthy }
+  end
+
+  context '2008年の場合' do
+    let(:year) { 2008 }
+    it { is_expected.to be_truthy }
+  end
+
+  context '2016年の場合' do
+    let(:year) { 2016 }
+    it { is_expected.to be_truthy }
+  end
+
+  context '1984年の場合' do
+    let(:year) { 1984 }
+    it { is_expected.to be_truthy }
+  end
 end
