294b4c6529f38c7eb9eac3842c32e0d2fbfb6c85
haazime <h4zime@gmail.com>
2014-09-06 19:09:56 +0900

    divisible_by{N}?をdivisible_by?(N)に変更

--- lib/my_year.rb
+++ lib/my_year.rb
@@ -6,21 +6,13 @@ class MyYear
 
   def leap?
     return false if @year.nil?
-    return false if divisible_by_100? && !divisible_by_400?
-    divisible_by_4?
+    return false if divisible_by?(100) && !divisible_by?(400)
+    divisible_by?(4)
   end
 
 private
 
-  def divisible_by_4?
-    @year % 4 == 0
-  end
-
-  def divisible_by_100?
-    @year % 100 == 0
-  end
-
-  def divisible_by_400?
-    @year % 400 == 0
+  def divisible_by?(num)
+    @year % num == 0
   end
 end
