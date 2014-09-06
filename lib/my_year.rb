9f8cf3d9408ec82fcb0410b595f6e9729b5d7085
haazime <h4zime@gmail.com>
2014-09-06 18:45:56 +0900

    プロダクトコードをリファクタリング

--- lib/my_year.rb
+++ lib/my_year.rb
@@ -5,7 +5,21 @@ class MyYear
   end
 
   def leap?
-    return false if @year % 100 == 0 && @year % 400 > 0
+    return false if divisible_by_100? && !divisible_by_400?
+    divisible_by_4?
+  end
+
+private
+
+  def divisible_by_4?
     @year % 4 == 0
+  end
+
+  def divisible_by_100?
+    @year % 100 == 0
+  end
+
+  def divisible_by_400?
+    @year % 400 == 0
   end
 end
